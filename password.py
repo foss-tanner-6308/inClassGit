cb822a162b8bf94ec77495aee164eef3665dff58
Tanner <fossta@oregonstate.edu>
2021-04-15 18:11:20 -0700

    fixed user input

--- password.py
+++ password.py
@@ -1,9 +1,13 @@
 import random
 import string
 
+a = int(input("Enter number: "))
 
-def pw():
-    a = input("Enter a number: ")
+
+def pw(x):
     chars = string.ascii_letters
     p = "".join(random.choice(chars) for i in range(a))
     print(p)
+
+
+pw(a)
